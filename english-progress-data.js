599cacd8f3cfc21872971e964c54d5b036b804ab
Terereboy <131317663+Terereboy@users.noreply.github.com>
2026-09-13 09:17:49 -0300

    Add English progress data source

---
+++ english-progress-data.js
@@ -0,0 +1,11 @@
+window.englishProgress = {
+  currentLevel: null,
+  progressFromBaseline: 0,
+  lastAssessment: null,
+  history: [],
+  dailyPractice: {
+    date: null,
+    percent: 0,
+    label: "No practice today"
+  }
+};
